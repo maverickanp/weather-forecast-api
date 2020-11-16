2b7fe33c9b5aabaad052c69d3ac346169a46e19a
Artur Pedrosa <apedrosa.dev@gmail.com>
2020-11-16 01:11:48 -0300

    fix: add values from parameters to test api client

--- src/openWeatherClient.js
+++ src/openWeatherClient.js
@@ -5,12 +5,14 @@ const WEATHER_PUB_API = process.env.WEATHER_PUB_API
 const API_KEY = process.env.API_KEY
 
 const weatherForecast = async (location, date) => {
-  // async getRates (baseCurrency: string): Promise<RateResponse> {
-const lat = '33.441792'
-const lon = '-94.037689'
+//const lat = '33.441792'
+//const lon = '-94.037689'
   try {
-    // const response = await axios.get(WEATHER_PUB_API + `latest?base=${baseCurrency}`)
-    const response = await axios.get(WEATHER_PUB_API + `?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    //const response = await axios.get(WEATHER_PUB_API + `?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    const response = await axios.get(WEATHER_PUB_API + `?q=${location}&appid=${API_KEY}`)
+    
+    console.log('DATE:',date)
+    console.log('DATA:',response.data)
     return response.data
   } catch (error) {
     throw new Error(error.response.data.error)
