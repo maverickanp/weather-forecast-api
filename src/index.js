786522a94ed5581eac064cd8c2ed785d20c360a8
Artur Pedrosa <apedrosa.dev@gmail.com>
2020-11-15 23:34:42 -0300

    chore: add body-parser for json response

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
 
 app.post('/weatherforecast', (request, response) => {
   console.log(request.body)
