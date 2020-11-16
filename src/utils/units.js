caffe2b84b9bee88789a2757aef4151081555dd0
Artur Pedrosa <apedrosa.dev@gmail.com>
2020-11-16 17:57:58 -0300

    feat: add utils to undestand units

---
+++ src/utils/units.js
@@ -0,0 +1,13 @@
+
+const stringCheckUnits = (units) => {
+  switch (units) {
+    case 'metric':
+      return 'Celsius'
+    case 'imperial':
+      return 'Fahrenheit'
+    default:
+      return 'invalid input'
+  }
+}
+
+module.exports = stringCheckUnits
