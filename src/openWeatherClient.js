960a0bcc72933b9d444293cb1f57049bad7d18d6
Artur Pedrosa <apedrosa.dev@gmail.com>
2020-11-16 17:59:07 -0300

    feat: add units and lang to request api

--- src/openWeatherClient.js
+++ src/openWeatherClient.js
@@ -2,10 +2,12 @@ const axios = require('axios')
 
 const WEATHER_PUB_API = process.env.WEATHER_PUB_API
 const API_KEY = process.env.API_KEY
+const LANG = process.env.LANG
+const UNITS = process.env.UNITS
 
 const weatherForecast = async (location, date) => {
   try {
-    const response = await axios.get(WEATHER_PUB_API + `?q=${location}&appid=${API_KEY}`)
+    const response = await axios.get(WEATHER_PUB_API + `?q=${location}&appid=${API_KEY}&units=${UNITS}&lang=${LANG}`)
 
     return response.data.list[0].main
   } catch (error) {
