5a4047e73a7454a55e6e07a42ddb1d40bf6c1f90
Artur Pedrosa <apedrosa.dev@gmail.com>
2020-11-18 17:20:11 -0300

    fix: test action on df

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const utils = require('./utils/units')
 const weatherGroupsTranslate = require('./utils/weatherVariations')
+const detectIntent = require('./utils/intents')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const UNITS = utils.stringCheckUnits(process.env.UNITS)
@@ -20,8 +21,9 @@ app.get('/', function (_, res) {
 app.post('/weatherforecastDF', async (request, response) => {
   const location = request.body.queryResult.parameters.location.city
   const time = request.body.queryResult.parameters['date-time'] || new Date()
-  // weatherRain | weatherSun |
+  // weatherRain | weatherSun | weather
   const action = request.body.queryResult.action
+  console.log(action)
 
   try {
     const message = await weatherForecast(location, time)
@@ -34,19 +36,25 @@ app.post('/weatherforecastDF', async (request, response) => {
 })
 
 app.post('/weatherforecast', async (request, response) => {
-  const location = request.body.queryResult.parameters.location.city
-  const time = request.body.queryResult.parameters['date-time'] || new Date()
+  // POST https://dialogflow.googleapis.com/v2/projects/project-id/agent/sessions/session-id:detectIntent
+  // Project ID = 'weather-forecast-orff'
+  // AGENT_LANGUAGE = "pt-br";
+  // AGENT_ID = "b23060bb-8093-40ee-83bf-099eab6bb119";
+  // AGENT_AVATAR_ID = "https://www.gstatic.com/dialogflow-console/common/assets/img/logo-short.png";
+  // SERVICE_BASE_URL = "https://console.dialogflow.com/api-client/";
+  // BOT_SERVICE_BASE_URL = "https://bots.dialogflow.com";
+  // V2_ENABLED = true;
+
+  // const location = request.body.queryResult.parameters.location.city
+  // const time = request.body.queryResult.parameters['date-time'] || new Date()
   // weatherRain | weatherSun |
-  const action = request.body.queryResult.action
+  // const action = request.body.queryResult.action
 
-  try {
-    const message = await weatherForecast(location, time)
-    const iconUrl = await weatherIcon(message.weather.icon)
+  const user = request.body.user
+  const query = request.body.query
 
-    response.json({ fulfillmentText: `A temperatura está ${message.temp}${UNITS} graus em ${location} ${iconUrl} \n ${action}` })
-  } catch (error) {
-    response.status(500).json({ error: error })
-  }
+  detectIntent.executeQueries('weather-forecast-orff', user, query, 'pt-br')
+  response.json({ process: 'DONE' })
 })
 
 // {
